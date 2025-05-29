351f2dc04d6a27703a1b380fe72dbf702bfc6b3c
Jimmyfitz3 <brendafitzpatrick107@gmail.com>
2025-05-29 13:17:06 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuto.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+@Config
+@Autonomous(name = "testAuto", group = "Autonomous")
+public class testAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .strafeTo(new Vector2d(10, 20))
+                .waitSeconds(3)
+                .turn(Math.toRadians(180));
+
+        Action trajectoryActionChosen = tab1.build();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen
+                )
+        );
+    }
+}
